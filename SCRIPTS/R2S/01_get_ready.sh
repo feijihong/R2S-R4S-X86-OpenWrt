c52c3acd5158ba8f2ce9f67150c535b96413c8d9
feijihong <feijihong5@gmail.com>
2021-01-30 15:43:04 +0800

    Update 01_get_ready.sh

--- SCRIPTS/R2S/01_get_ready.sh
+++ SCRIPTS/R2S/01_get_ready.sh
@@ -5,5 +5,5 @@ git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-
 git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
 git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 exit 0
